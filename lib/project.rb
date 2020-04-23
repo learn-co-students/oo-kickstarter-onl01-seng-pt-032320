c22eb482d3bf0507aa948846e94b4dad2ab150bd
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-23 07:50:44 +0000

    Done.

--- lib/project.rb
+++ lib/project.rb
@@ -11,9 +11,14 @@ class Project
     @@all << self
   end
   
-  def add_backer(name)
-    self.backers << name
-    binding.pry
+  def add_backer(backer_obj)
+    self.backers << backer_obj
+    #binding.pry
+    #Project.all.select{ |p| p.add_backer(self) if self.backed_projects.include?(p)}
+    #Backer.all.select{ |b| b.back_project(self) if b.backed_projects.include?(self)}
+    #Backer.all.select{ |b| b.back_project(b) if self.backers.include?(b) }
+    #Backer.all.select{ |b| b.back_project(self) if self.backers.name.include?(b.name)}
+    #Backer.all.select{ |b| b.back_project(self) if b==backer_obj }
   end
   
   def self.all
@@ -22,3 +27,6 @@ class Project
   
   
 end
+#adds the project to the backer's backed_projects array
+#SELF - Project
+#TAKE BACKER_OBJ AND MAKE IT BACK SELF-PROJECT
